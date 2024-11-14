ad248137d93e4245c18a12dd3e5e603bd0786310
sumitha-153 <126260685+sumitha-153@users.noreply.github.com>
2024-11-14 18:06:34 +0530

    Second changes

--- src/pages/api/favorites.js
+++ src/pages/api/favorites.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     const favorites = user.favorites || [];
     if (favorites.includes(productId)) {
-      // Remove from favorites
       await db.collection('users').updateOne(
         { name },
         { $pull: { favorites: productId } }
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -38,7 +38,40 @@ export default async function handler(req, res) {
       console.error('Failed to update user info:', error);
       res.status(500).json({ message: 'Failed to update user info' });
     }
-  } else {
+  }else if( req.method === 'POST'){
+    const {address,city,postalCode,country,state,userName}=req.body;
+    console.log("Inside User.js");
+    console.log(req.body);
+
+    const user =await db.collection('users').findOne({name:userName});
+    if(!user){
+      return res.status(404).json({message:'User not found'});
+    }
+    try {
+      const result = await db.collection('users').updateOne(
+        {name: userName},
+        {
+          $push: {
+            shippingInfo: {
+              address,
+              city,
+              postalCode,
+              country,
+              state,
+            },
+          },
+        }
+      );
+      if (result.matchedCount === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.status(200).json({ message: 'User information updated successfully' });
+    } catch (error) {
+      console.error('Failed to update user info:', error);
+      res.status(500).json({ message: 'Failed to update user info' });  
+   } 
+  }
+  else {
     res.setHeader('Allow', ['GET', 'PUT']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
